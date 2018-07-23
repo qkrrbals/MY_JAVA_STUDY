1610ea17760f821ee17cc67b8313a0b70f1ce403
admin <admin@10.137.138.157>
2018-07-23 12:33:30 +0900

    utf-8로 인코딩형식을 바꿈

--- src/icehs/science/chapter02/Helloqkr.java
+++ src/icehs/science/chapter02/Helloqkr.java
@@ -1,9 +1,9 @@
 package icehs.science.chapter02;
 
-public class Hellopark {
+public class Helloqkr {
 
 	public static void main(String[] args) {
-		System.out.println("안녕하세요?");
+		System.out.println("Hello?");
 
 	}
 
